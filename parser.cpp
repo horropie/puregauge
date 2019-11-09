d254bee41d2d26bb9791f4061db60bf4eb602942
horropie <31204217+horropie@users.noreply.github.com>
2019-11-09 22:26:12 +0100

    First steps for the plaquette program.

--- parser.cpp
+++ parser.cpp
@@ -3,7 +3,8 @@
 
 void setlattice(int (&dimv)[3]){
 
-	/** We have to check if the input is of integer type. Note that this (unfortunately) accepts things like "10abc", as its accepts the number and ignores the rest.**/
+	/** We have to check if the input is of integer type. Note that this (unfortunately) accepts things like "10abc", as its accepts the number and ignores the rest. 
+    One could split a header isinteger.h from this to make it simpler.**/
 	int isinteger;
 
 	std::cout << "What is the size of the lattice in x-direction?" << std::endl;
---
+++ plaquette.cpp
@@ -0,0 +1,12 @@
+/** A code to calculate the plaquette at a certain point on the lattice. It should be compatible with then integrating over the whole lattice. 
+The input is a random matrix U and the lattice array. The output is the plaquette for a certain point on the lattice.**/
+
+#include <iostream>
+#include "Eigen/Eigen/Dense"
+
+/** A problem is, that the size of the lattice has to be known for this definition to work. **/
+void plaquette(double &(lattice)[][][][], double &(matrix)[2][2],){
+
+
+}
+
