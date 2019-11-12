e483951d6bde93cff08f9cdc1358687cef5f7686
horropie <31204217+horropie@users.noreply.github.com>
2019-11-12 14:53:12 +0100

    adj->adjoint

--- plaquette.cpp
+++ plaquette.cpp
@@ -23,9 +23,9 @@ double plaquette(lattice l){
     for(int x = 0; x < 1; x++){
         for (int y = 0; y < 2; y++){
             for (int z = 0; z < 4; z++){
-                  xyplaquettevalue = l.links[x][y][z][1].adj() * l.links[x][y+1][z][0].adj() * l.links[x+1][y][z][1] * l.links[x][y][z][0];
-                  xzplaquettevalue = l.links[x][y][z][2].adj() * l.links[x][y][z+1][0].adj() * l.links[x+1][y][z][2] * l.links[x][y][z][0];
-                  yzplaquettevalue = l.links[x][y][z][2].adj() * l.links[x][y][z+1][1].adj() * l.links[x][y+1][z][2] * l.links[x][y][z][1];
+                  xyplaquettevalue = l.links[x][y][z][1].adjoint() * l.links[x][y+1][z][0].adjoint() * l.links[x+1][y][z][1] * l.links[x][y][z][0];
+                  xzplaquettevalue = l.links[x][y][z][2].adjoint() * l.links[x][y][z+1][0].adjoint() * l.links[x+1][y][z][2] * l.links[x][y][z][0];
+                  yzplaquettevalue = l.links[x][y][z][2].adjoint() * l.links[x][y][z+1][1].adjoint() * l.links[x][y+1][z][2] * l.links[x][y][z][1];
 
                   plaquette += xyplaquettevalue.trace().real() + xzplaquettevalue.trace().real() + yzplaquettevalue.trace().real();
 
