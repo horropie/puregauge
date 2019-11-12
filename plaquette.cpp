2c187862f08331849ed59cbc367f88a72c0912f5
horropie <31204217+horropie@users.noreply.github.com>
2019-11-12 01:13:50 +0100

    Loop for plaquette. Output still missing.

--- plaquette.cpp
+++ plaquette.cpp
@@ -1,5 +1,5 @@
 /** A code to calculate the plaquette at a certain point on the lattice. It should be compatible with then integrating over the whole lattice. 
-The input is a random matrix U and the lattice array. The output is the plaquette for a certain point on the lattice.**/
+The input is a random matrix U and the lattice array. The output is all the plaquette values for points on the lattice.**/
 
 #include <stdio.h>
 #include <iostream>
@@ -10,10 +10,23 @@ The input is a random matrix U and the lattice array. The output is the plaquett
 #include <boost/math/constants/constants.hpp>
 #include "Eigen/Eigen/Dense"
 
-/** A problem is, that the size of the lattice has to be known for this definition to work.
-Can we really apply our Plaquette formula to the xyz case (It seems to be just a xy case)?  
-Also, how do we safe the matrices that correspond to the links? Do we construct them on the fly?**/
-void plaquette(double &(lattice)[][][][3], double &(matrix)[2][2],){
-    /**Pseudocode: U(links[x][y][z][1]).adj() * U(links[x][y+1][z][0]).adj() * U(links[x+1][y][z][1]) * U(links[x][y][z][0])**/  
+/** A problem is, that the size of the lattice has to be known for this definition to work. 
+Apparently std::vector can have dynamic size, maybe there is somethings similiar for general arrays.**/
+double plaquette(double &(links)[][][][3], double &(matrix)[2][2], int (&dimv)[3]){
+
+    double xyplaquettevalue, xzplaquettevalue, yzplaquettevalue;
+
+    for(int x = 0; x < dimv[0]; x++){
+        for (int y = 0; y < dimv[1]; y++){
+            for (int z = 0; z < dimv[2]; z++){
+                  xyplaquettevalue = links[x][y][z][1].adj() * links[x][y+1][z][0].adj() * links[x+1][y][z][1] * links[x][y][z][0];
+                  xzplaquettevalue = links[x][y][z][1].adj() * links[x][y][z+1][0].adj() * links[x+1][y][z][1] * links[x][y][z][0];
+                  yzplaquettevalue = links[x][y][z][1].adj() * links[x][y][z+1][0].adj() * links[x][y+1][z][1] * links[x][y][z][0];
+
+                  /**What output do we want? Single doubles or whole arrays? Or saving?**/
+              }  
+           }   
+        }
+    
 }
 
