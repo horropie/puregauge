b9ab8d35e22f3c6578acb3ea2c2b68fa17c22fab
LPenno <57483023+LPenno@users.noreply.github.com>
2019-11-23 11:51:52 +0100

    3d metropolis

--- main.cpp
+++ main.cpp
@@ -1,109 +1,179 @@
 #include <iostream>
+#include "lattice.h"
+#include <Eigen/Dense>
 #include <random>
-#include <math.h>
+#include <vector>
+#include <complex>
+#include <boost/math/constants/constants.hpp>
 #include <fstream>
-
 using namespace std;
-double f;
-fstream file;
+//definiere Parameter (Anzahl Wdh., Temperatur, Zufallsgenerator)
+static int rep=10;
+double beta= 0.7;
+double N =1.0;
 
-std::default_random_engine generator;
-std::uniform_real_distribution<double> uni_real_distr(0.0,1.0);
+Eigen::Matrix<std::complex<double>,2,2> test;
+Eigen::Matrix<std::complex<double>,2,2> temp;
+
+fstream f;
+
+double alt=0.0;
+double neu=0.0;
+double p;
 
 
 
-double function(double x)
+std::default_random_engine Generator;
+std::uniform_real_distribution<double> uni_real_dist(0.0,1.0);
+
+// Methode zum Erzeugen von Zufallsmatrix
+Eigen::Matrix<std::complex<double>,2,2> zuf()
 {
-    f= 3*x*x;
-    return  f;
+    Eigen::Matrix<std::complex<double>,2,2> randm;
+    double rand[3];
+	double norm(2.0);
+	while(norm>1.0){
+     rand[0]=uni_real_dist(Generator);
+     rand[1]=uni_real_dist(Generator);
+     rand[2]=uni_real_dist(Generator);
+	 norm=sqrt(rand[0]*rand[0]+rand[1]*rand[1]+rand[2]*rand[2]);
+	} // random direction on 3 sphere
+	rand[0]/=norm;
+	rand[1]/=norm;
+	rand[2]/=norm;
+	const double phase(2.0*3.1415*uni_real_dist(Generator));
+	const double cosn(cos(phase));
+	const double sinn(sin(phase));
+	randm(0,0)=std::complex<double>(cosn,0.0)+std::complex<double>(0.0,sinn*rand[2]);
+	randm(0,1)=std::complex<double>(0.0,sinn*rand[0])+std::complex<double>(sinn*rand[1],0.0);
+	randm(1,1)=conj(randm(0,0));
+	randm(1,0)=-conj(randm(0,1));
+	return randm;
 }
 
-double rectint(double start,double finish, int step)
-{
-    double area=0.0;
-    double interval= finish-start;
-    double addstep=interval/step;
-    //cout<< addstep<<endl;
-    for(int i=0;i<step; i=i+1 ){
-            //cout << start+(i*addstep)<<endl;
-        area += addstep*(function(start+(i*addstep)));
-    }
-    return area;
-}
-double simpson(double start,double finish, int step){
-    double area=0.0;
-    double interval= finish-start;
-    double addstep=interval/step;
-
-    for(int i=1;i<=step/2;i++){
-        area+=(addstep/3.0)*(function(start+addstep*(2*i-2))+(4*function(start+addstep*(2*i-1)))+function(start+addstep*(2*i)));
-    }
-    return area;
-}
-
-double montecarlo(int rep, int dimension){
-
-    double in=0.0;
-    double area=0.0;
-    //double rand[dimension];
-    double sum=0.0;
 
 
-    for(int i =0; i<rep;i++){
-            for(int j=0;j<dimension;j++){
-              //rand[j]= ;
-               sum=sum+pow(uni_real_distr(generator),2);
-            }
-
-        if(sum<=1)
-            in=in+1.0;
-        sum=0.0;
-
-    }
-    area=pow(2.0,dimension)*(in/rep);
-    return area;
-
-}
-int faculty(int number){
-    if(number ==1)
-        return number;
-    else
-        return number*faculty(number-1);
-
-}
- int double_fac(int number){
-    if(number ==1)
-        return number;
-    else
-        return number*double_fac(number-2);
-
- }
 
 int main()
 {
-  double calculate_even;
-  double calculate_odd;
-  int dim_odd;
-  int dim_even;
+    lattice l ;
+     string datei= "Metropolis.txt";
+        f.open(datei,ios::out);
+       f << "O" << endl;
 
-    //cout << montecarlo(1000,4)<< endl;
-    string datei= "montecarlo.txt";
-        file.open(datei,ios::out);
-       file << "dimension;calculated;compare" << endl;
-       for(int l=1;l<=10;l=l+1){
-            dim_even=2*l;
-            dim_odd=(2*l)+1;
-         calculate_even=pow(3.1415,l)/faculty(l);
-         calculate_odd=(pow(2,l+1)*pow(3.1415,l))/double_fac(dim_odd);
-         //cout << calculate_even<<endl;
-         //cout<< calculate_odd<<endl;
+    // Berechne die Summe der Plaquettes des Gitters, speichere in plaquette
 
-        file<<to_string(dim_even)+";"+to_string(montecarlo(10000,dim_even))+";"+to_string(calculate_even)<<endl;
-        file<<to_string(dim_odd)+";"+to_string(montecarlo(10000,dim_odd))+";"+to_string(calculate_odd)<<endl;
-       }
-       file.close();
-        //cout<<double_fac(5);
+    double plaquette=0.0;
+    Eigen::Matrix<std::complex<double>,2,2> xyplaquettevalue, xzplaquettevalue, yzplaquettevalue;
+
+     for(int x = 0; x < l.xdim; x++){
+        for (int y = 0; y < l.ydim; y++){
+            for (int z = 0; z < l.zdim; z++){
+                  xyplaquettevalue = l.links[x][y][z][1].adjoint() * l.links[x][(y+1)%l.ydim][z][0].adjoint() * l.links[(x+1)%l.xdim][y][z][1] * l.links[x][y][z][0];
+                  xzplaquettevalue = l.links[x][y][z][2].adjoint() * l.links[x][y][(z+1)%l.zdim][0].adjoint() * l.links[(x+1)%l.xdim][y][z][2] * l.links[x][y][z][0];
+                  yzplaquettevalue = l.links[x][y][z][2].adjoint() * l.links[x][y][(z+1)%l.zdim][1].adjoint() * l.links[x][(y+1)%l.ydim][z][2] * l.links[x][y][z][1];
+                  plaquette += (xyplaquettevalue.trace().real() +xzplaquettevalue.trace().real()+yzplaquettevalue.trace().real());
+
+            }}}
+            //Beginne Metroplolis
+      for(int i =0; i<rep;i++){
+
+          //Gehe linear durchs Gitter
+             for(int x = 0; x < l.xdim; x++){
+                for (int y = 0; y < l.ydim; y++){
+                    for (int z = 0; z < l.zdim; z++){
+            //Erzeuge Zufallsmatrix für x-Richtung
+                        test=zuf();
+                        //Berechne die alten (alt) und neuen (neu) Plaquette-Werte
+                        // Alte Plaquettes in xy Richtung
+                        temp=(l.links[x][y][z][0]*l.links[(x+1)%l.xdim][y][z][1]*l.links[x][(y+1)%l.ydim][z][0].adjoint()*l.links[x][y][z][1].adjoint())+(l.links[x][y][z][0]*l.links[(x+1)%l.xdim][(y-1)%l.ydim][z][1].adjoint()*l.links[x][(y-1)%l.ydim][z][0].adjoint()*l.links[x][(y-1)%l.ydim][z][1]);
+                       // Alte Plaquettes in xz
+                        temp+=(l.links[x][y][z][0]*l.links[(x+1)%l.xdim][y][z][2]*l.links[x][y][(z+1)/l.zdim][0].adjoint()*l.links[x][y][z][2].adjoint())+(l.links[x][y][z][0]*l.links[(x+1)%l.xdim][y][(z-1)%l.zdim][2].adjoint()*l.links[x][y][(z-1)%l.zdim][0].adjoint()*l.links[x][y][(z-1)%l.zdim][2]);
+
+                        alt=temp.trace().real();
+                        //Zu prüfende Plaquettes in xy
+                        temp=(test*l.links[(x+1)%l.xdim][y][z][1]*l.links[x][(y+1)%l.ydim][z][0].adjoint()*l.links[x][y][z][1].adjoint())+(test*l.links[(x+1)%l.xdim][(y-1)%l.ydim][z][1].adjoint()*l.links[x][(y-1)%l.ydim][z][0].adjoint()*l.links[x][(y-1)%l.ydim][z][1]);
+
+                        //zu prüfende in xz
+                         temp+=(test*l.links[(x+1)%l.xdim][y][z][2]*l.links[x][y][(z+1)/l.zdim][0].adjoint()*l.links[x][y][z][2].adjoint())+(test*l.links[(x+1)%l.xdim][y][(z-1)%l.zdim][2].adjoint()*l.links[x][y][(z-1)%l.zdim][0].adjoint()*l.links[x][y][(z-1)%l.zdim][2]);
+                        neu=temp.trace().real();
+                        //Akzeptieren oder ablehnen
+                        if(exp((-beta/N)*(neu-alt))>=1){
+                            l.update(x,y,z,0,test);
+                            plaquette=plaquette-alt+neu;
+                        }
+                        else{
+                           p=uni_real_dist(Generator);
+                           if (exp((-beta/N)*(neu-alt))>=p){
+                                l.update(x,y,z,0,test);
+                                plaquette=plaquette-alt+neu;
+
+                           }
+                        }
+
+                        // Das Gleiche für y
+                         test=zuf();
+                        //Berechne die alten (alt) und neuen (neu) Plaquette-Werte
+                        // Alte Werte in xy
+                        temp=(l.links[x][y][z][1]*l.links[x][(y+1)%l.ydim][z][0]*l.links[(x+1)%l.xdim][y][z][1].adjoint()*l.links[x][y][z][0].adjoint())+(l.links[x][y][z][1]*l.links[(x-1)%l.xdim][(y+1)%l.ydim][z][0].adjoint()*l.links[(x-1)%l.xdim][y][z][1].adjoint()*l.links[(x-1)%l.xdim][y][z][0]);
+                       // in yz
+                       temp+=(l.links[x][y][z][1]*l.links[x][(y+1)%l.ydim][z][2]*l.links[x][y][(z+1)%l.zdim][1].adjoint()*l.links[x][y][z][2].adjoint())+(l.links[x][y][z][1]*l.links[x][(y+1)%l.ydim][(z-1)%l.zdim][2].adjoint()*l.links[x][y][(z-1)%l.zdim][1].adjoint()*l.links[x][y][(z-1)%l.zdim][2]);
+                        alt=temp.trace().real();
+                        // Zu prüfende in xy
+                        temp=(test*l.links[x][(y+1)%l.ydim][z][0]*l.links[(x+1)%l.xdim][y][z][1].adjoint()*l.links[x][y][z][0].adjoint())+(test*l.links[(x-1)%l.xdim][(y+1)%l.ydim][z][0].adjoint()*l.links[(x-1)%l.xdim][y][z][1].adjoint()*l.links[(x-1)%l.xdim][y][z][0]);
+                       // in yz
+                        temp+=(test*l.links[x][(y+1)%l.ydim][z][2]*l.links[x][y][(z+1)%l.zdim][1].adjoint()*l.links[x][y][z][2].adjoint())+(test*l.links[x][(y+1)%l.ydim][(z-1)%l.zdim][2].adjoint()*l.links[x][y][(z-1)%l.zdim][1].adjoint()*l.links[x][y][(z-1)%l.zdim][2]);
+
+                        neu=temp.trace().real();
+                        //Akzeptieren oder ablehnen
+                        if(exp((-beta/N)*(neu-alt))>=1){
+                            l.update(x,y,z,1,test);
+                            plaquette=plaquette-alt+neu;
+                        }
+                        else{
+                           p=uni_real_dist(Generator);
+                           if (exp((-beta/N)*(neu-alt))>=p){
+                                l.update(x,y,z,1,test);
+                                plaquette=plaquette-alt+neu;
+
+                           }
+                        }
+
+                        // Das Gleiche für z
+                         test=zuf();
+                        //Berechne die alten (alt) und neuen (neu) Plaquette-Werte
+                        // Alte Werte in xz
+                        temp=(l.links[x][y][z][2]*l.links[x][y][(z+1)%l.zdim][0]*l.links[(x+1)%l.xdim][y][z][2].adjoint()*l.links[x][y][z][0].adjoint())+(l.links[x][y][z][2]*l.links[(x-1)%l.xdim][y][(z+1)%l.zdim][0].adjoint()*l.links[(x-1)%l.xdim][y][z][2].adjoint()*l.links[(x-1)%l.xdim][y][z][0]);
+                       // in yz
+                      temp+=(l.links[x][y][z][2]*l.links[x][y][(z+1)%l.zdim][1]*l.links[x][(y+1)%l.ydim][z][2].adjoint()*l.links[x][y][z][1].adjoint())+(l.links[x][y][z][2]*l.links[x][(y-1)%l.ydim][(z+1)%l.zdim][1].adjoint()*l.links[x][(y-1)%l.ydim][z][2].adjoint()*l.links[x][(y-1)%l.ydim][z][1]);
+                        alt=temp.trace().real();
+                        // Zu prüfende in xy
+                         temp=(test*l.links[x][y][(z+1)%l.zdim][0]*l.links[(x+1)%l.xdim][y][z][2].adjoint()*l.links[x][y][z][0].adjoint())+(test*l.links[(x-1)%l.xdim][y][(z+1)%l.zdim][0].adjoint()*l.links[(x-1)%l.xdim][y][z][2].adjoint()*l.links[(x-1)%l.xdim][y][z][0]);
+                       // in yz
+                      temp+=(test*l.links[x][y][(z+1)%l.zdim][1]*l.links[x][(y+1)%l.ydim][z][2].adjoint()*l.links[x][y][z][1].adjoint())+(test*l.links[x][(y-1)%l.ydim][(z+1)%l.zdim][1].adjoint()*l.links[x][(y-1)%l.ydim][z][2].adjoint()*l.links[x][(y-1)%l.ydim][z][1]);
+
+                        neu=temp.trace().real();
+                        //Akzeptieren oder ablehnen
+                        if(exp((-beta/N)*(neu-alt))>=1){
+                            l.update(x,y,z,2,test);
+                            plaquette=plaquette-alt+neu;
+                        }
+                        else{
+                           p=uni_real_dist(Generator);
+                           if (exp((-beta/N)*(neu-alt))>=p){
+                                l.update(x,y,z,2,test);
+                                plaquette=plaquette-alt+neu;
+
+                           }
+                        }
 
 
+                    }}}
+
+
+//Messwert speichern
+ f <<plaquette<< endl;
+      }
+f.close();
     return 0;
 }
