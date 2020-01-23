053e819f4c19593a816c3dd83eaae28a42f0e688
Lpenno <Lena.Maria.Penno@uni-jena.de>
2020-01-23 10:12:32 +0100

    Zeiger ergänzt

--- lattice.cpp
+++ lattice.cpp
@@ -1,5 +1,5 @@
 #include "lattice.h"
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 #include <random>
 #include <vector>
 #include <complex>
@@ -10,36 +10,39 @@ using namespace std;
 std::default_random_engine generator;
 std::uniform_real_distribution<double> uni_real_distr(0.0,1.0);
 
-lattice::lattice()
+lattice::lattice(int space,int t)
 {
-xdim= 5;
-ydim= 5;
-zdim= 5;
-tdim= 20;
+xdim=space;
+ydim=space;
+zdim=space;
+tdim=t;
 
 
+links = new Eigen::Matrix<std::complex<double>,2,2>****[xdim];
 
-
-    for(int t =0;t<tdim;t++){
         for(int i =0;i<xdim;i++){
-            for(int j =0;j<ydim;j++){
-                for(int k=0;k<zdim;k++){
+                links[i]= new Eigen::Matrix<std::complex<double>,2,2>***[ydim];
+                for(int j =0;j<ydim;j++){
+                    links[i][j]= new Eigen::Matrix<std::complex<double>,2,2>**[zdim];
+                    for(int k=0;k<zdim;k++){
+                        links[i][j][k]=new Eigen::Matrix<std::complex<double>,2,2>*[4];
+                        for(int r=0;r<4;r++){
+                            links[i][j][k][r]=new Eigen::Matrix<std::complex<double>,2,2>[tdim];
+                            for(int t=0;t<tdim;t++){
+                                links[i][j][k][r][t]=rand();
+              }
+              }
+              }
+              }
+              }
 
-                    links[i][j][k][0][t]=rand();
-                    links[i][j][k][1][t]=rand();
-                    links[i][j][k][2][t]=rand();
-                }
-            }
 
-        }
-
-    }    
 
 }
 
 lattice::~lattice()
 {
-    //dtor
+
 }
 void lattice::update(int xpos, int ypos, int zpos, int direction, int time, Eigen::Matrix<std::complex<double>,2,2> newMatrix)
 {
@@ -47,7 +50,7 @@ void lattice::update(int xpos, int ypos, int zpos, int direction, int time, Eige
 }
 Eigen::Matrix<std::complex<double>,2,2> lattice::rand()
 {
-    Eigen::Matrix<std::complex<double>,2,2> randm;
+Eigen::Matrix<std::complex<double>,2,2> randm;
     double rand[3];
 	double norm(2.0);
 	while(norm>1.0){
@@ -59,14 +62,18 @@ Eigen::Matrix<std::complex<double>,2,2> lattice::rand()
 	rand[0]/=norm;
 	rand[1]/=norm;
 	rand[2]/=norm;
+//cout<<"rand: "<<rand<<endl;
 	const double phase(2.0*3.1415*uni_real_distr(generator));
+	//cout<<"phase: "<<phase<<endl;
 	const double cosn(cos(phase));
 	const double sinn(sin(phase));
+	//cout<<"cosn: "<<cosn<<endl;
+	//cout<<"sinn: "<< sinn<<endl;
 	randm(0,0)=std::complex<double>(cosn,0.0)+std::complex<double>(0.0,sinn*rand[2]);
 	randm(0,1)=std::complex<double>(0.0,sinn*rand[0])+std::complex<double>(sinn*rand[1],0.0);
 	randm(1,1)=conj(randm(0,0));
 	randm(1,0)=-conj(randm(0,1));
 	return randm;
+
+
 }
-
-
--- lattice.h
+++ lattice.h
@@ -1,29 +1,30 @@
 #ifndef LATTICE_H
 #define LATTICE_H
 
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 
 using namespace Eigen;
-
 class lattice
 {
     public:
-        lattice();
-        int xdim;
-        int ydim;
-        int zdim;
-        int tdim;
+        lattice(int space,int t);
+         int xdim;
+         int ydim;
+         int zdim;
+         int tdim;
         double T;
         virtual ~lattice();
         void update(int xpos, int ypos, int zpos, int direction, int time, Eigen::Matrix<std::complex<double>,2,2> newMatrix);
         double energy(void);
         double plaquette(int x, int y, int z, int dir, int time);
-        Matrix<std::complex<double>,2,2> links[5][5][5][3][20] ;
+        Matrix<std::complex<double>,2,2> *****links;
+
 
     protected:
 
     private:
-        Matrix<std::complex<double>,2,2> rand(void);
+        Eigen::Matrix<std::complex<double>,2,2> rand( void);
+
 };
 
 #endif // LATTICE_H
